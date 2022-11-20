c282d0eca2aa3f16ea8c49936fca27115d40c746
odcenteno <pixie_cemodan@hotmail.com>
2022-11-20 16:32:11 -0700

    Initial commit learning testing w/ Jest

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,18 @@
+const { sum, multiply, divide } = require('./02-math');
+
+test('should add 2 + 2 to equal 4', () => {
+  const rta = sum(2, 2);
+  expect(rta).toBe(4);
+});
+test('should multiply 3 * 4 to equal 12', () => {
+  const rta = multiply(3, 4);
+  expect(rta).toBe(12);
+});
+test('should divide 9 / 3 to equal 3', () => {
+  const rta = divide(9, 3);
+  expect(rta).toBe(3);
+});
+test('should divide for zero to null', () => {
+  const rta = divide(2, 0);
+  expect(rta).toBeNull();
+});
