60648875a498726b0dd00f814367369046b05703
odcenteno <pixie_cemodan@hotmail.com>
2022-11-27 16:26:18 -0700

    faker problem solved on fakes branch

--- api/src/fakes/book.fake.js
+++ api/src/fakes/book.fake.js
@@ -9,10 +9,14 @@ const generateOneBook = () => ({
 const generateManyBooks = (size) => {
   const limit = size ?? 10;
   const fakeBooks = [];
-  for (let index = 0; index < limit.length; index += 1) {
+  for (let index = 0; index < limit; index += 1) {
     fakeBooks.push(generateOneBook());
   }
+  console.log(`Fake books response: ${fakeBooks}`);
   return [...fakeBooks];
 };
+
+const tryit = generateManyBooks(7);
+console.log(tryit);
 
 module.exports = { generateOneBook, generateManyBooks };
