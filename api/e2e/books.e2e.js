659e46a99f8182554e4d7e4420239720c7f15edb
odcenteno <pixie_cemodan@hotmail.com>
2022-11-27 18:03:11 -0700

    integration test for books with mocking

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const creatApp = require('../src/app');
+const { generateManyBooks } = require('../src/fakes/book.fake');
+
+const mockGetAll = jest.fn();
+
+// Método para suplantación mocking con jest
+jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
+
+describe('test for books endpoint', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = creatApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [get] /api/v1/books', () => {
+    test('should return a list of books', () => {
+      // Arrange
+      const fakeBooks = generateManyBooks(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
