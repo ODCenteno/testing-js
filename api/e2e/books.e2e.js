4b3227250428f36005534632f8cef5f9dc2e7252
odcenteno <pixie_cemodan@hotmail.com>
2022-11-29 21:29:01 -0700

    e2e test with mongodb

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,39 +1,55 @@
-const mockGetAll = jest.fn(); // subí la declaración para que no lanzara error
+const { MongoClient } = require('mongodb'); // traemos cliente de base de datos
 const request = require('supertest');
 const creatApp = require('../src/app');
-const { generateManyBooks } = require('../src/fakes/book.fake');
+const { config } = require('../src/config'); // Trae configuración para mongodb
 
-// Método para suplantación mocking con jest
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => {},
-})));
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('test for books endpoint', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = creatApp();
     server = app.listen(3001);
+    // preparando el cliente de la base de datos
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('test for [get] /api/v1/books', () => {
-    test('should return a list of books', () => {
-      // Arrange
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+    test('should return a list of books', async () => {
+      // Arrange - Preparando la semilla de datos
+      const seedData = await database.collection('books').insertMany([{
+        name: 'Book1',
+        year: 1998,
+        author: 'Daniel Centeno',
+      },
+      {
+        name: 'Book2',
+        year: 2004,
+        author: 'Pedrito Pinzón',
+      },
+      ]);
+      console.log(seedData);
       // Act
       return request(app)
-        .get('/api/v1/books')
+        .get('/api/v1/books') // importante comenzar con "/"
         .expect(200)
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
